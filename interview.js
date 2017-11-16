a7e9ba1e87dd36d7def924db24064b16a04595d6
bradcgarner <brad@bradgarner.com>
2017-11-15 23:07:07 -0500

    interview q done

--- interview.js
+++ interview.js
@@ -1,4 +1,5 @@
-import { BinarySearchTree } from "./binarytree";
+'use strict';
+const BinarySearchTree = require('./binarytree');
 
 'use strict';
 
@@ -14,20 +15,56 @@ import { BinarySearchTree } from "./binarytree";
 // output = bst
 
 // input
-// [1,2,3,4,5,6,7,8,9,10]
+const array = [0,1,2,3,4,5,6,7,8,9,10];
 
 // output
 //               5
 //       3              7
 //   2       4       6      8
 
-const converter = array => {
-  const binarySearchTree = new BinarySearchTree(); // each node is bst, so OK to call recursively
-  const midpoint   = Math.floor(array.length)/2;        // assume each time array is smaller, so 0 changes
-  const leftArray  = array.slice(0,midpoint);
-  const rightArray = array.slice()
-  binarySearchTree.insert(array[index])
-  converter(arraySubset);
+const converter = (array) => {
+  const tree = new BinarySearchTree();
+  let count = 0;
+  const looper = (start=0, end=array.length-1) => {      
+    const midpoint = Math.floor((end+start)/2); 
+    if ( start > end || start < 0 || end < 0) {
+      return;
+    } else if ( start === midpoint || end === midpoint ) {
+      tree.insert(array[midpoint], array[midpoint]);
+      return;
+    } else {
+      tree.insert(array[midpoint], array[midpoint]);
+      looper(start, midpoint - 1);
+      looper(midpoint + 1, end);
+    }
+  };
+  return looper();
+};
 
-  return binarySearchTree;
+const btree = converter(array);
+
+function inOrderPrint(tree, arr=[]) {
+  if(tree.left){
+    inOrderPrint(tree.left, arr);
+  }
+  arr.push(tree.value); 
+  if(tree.right) {
+    inOrderPrint(tree.right, arr);
+  }
+  return arr; 
 }
+
+console.log(inOrderPrint(btree));
+
+function preOrderPrint(tree, arr=[]) {
+  arr.push(tree.value); 
+  if(tree.left){
+    preOrderPrint(tree.left, arr);
+  }
+  if(tree.right) {
+    preOrderPrint(tree.right, arr);
+  }
+  return arr; 
+}
+
+console.log(preOrderPrint(btree));
